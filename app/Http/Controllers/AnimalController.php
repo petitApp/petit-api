5d2b634c81bec0991723cb9d1ba391c355280a32
AlRotten <alrodesis@gmail.com>
2020-01-30 20:25:26 +0100

    Admin-Animal Controllers Reorganization

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -42,6 +42,7 @@ class AnimalController extends Controller
         }
        return response($response,$response['code']);
     }
+
     public function updateAnimal(Request $request, $id)
     {
         $response = array('code' => 400, 'error_msg' => []);
@@ -84,6 +85,7 @@ class AnimalController extends Controller
         }
        return response($response,$response['code']);
     }
+    
     public function deleteAnimal(Request $request, $id)
     {
         $animal = Animals::find($id);
@@ -115,69 +117,5 @@ class AnimalController extends Controller
         }
        return response($response,$response['code']);
     }
-    public function getAnimals()
-    {
-        $response = array('code' => 400, 'error_msg' => []);
-        try {
-            $animals = Animals::all(['id', 'name', 'id_owner', 'id_type', 'sex', 'age', 'location', 'description', 'prefered_photo', 'id_breed']);
-            if (count($animals) > 0) {
-                $response = array('code' => 200, 'animals' => $animals);
-            } else {
-                $response = array('code' => 404, 'error_msg' => ['animals not found']);
-            }
-        } catch (\Exception $exception) {
-            $response = array('code' => 500, 'error_msg' => $exception->getMessage());
-        }
-        return view('updateanimal', ['responseAnimals' => $response]);
 
-    //Paginated request of all animals (Administrator) 
-    public function getAllAnimalsPaginated() 
-    {
-        try {
-            $animals = Animals::orderBy('id', 'asc')->paginate(10)->onEachSide(2);
-            $response = array('code' => 200, 'animals' => $animals);
-        } catch(\Exception $exception) {
-            $response = array('code' => 500, 'error_msg' => $exception->getMessage());
-        }
-
-        return view('defaultPaginated',['responseAnimals' => $response]);        
-    }
-
-
-    public function createAnimalAdmin(Request $request)
-    {
-        $response = array('code' => 400, 'error_msg' => []);
-        if (isset($request)) {
-            if (!$request->name) array_push($response['error_msg'], 'Name is required');
-            if (!$request->id_type) array_push($response['error_msg'], 'Type is required');
-            if (!$request->sex) array_push($response['error_msg'], 'Sex or gender is required');
-            if (!$request->age) array_push($response['error_msg'], 'Age is required');
-            if (!$request->location) array_push($response['error_msg'], 'Location is required');
-            if (!$request->description) array_push($response['error_msg'], 'Description is required');
-            if (!$request->prefered_photo) array_push($response['error_msg'], 'Prefered_photo is required');
-            if (!$request->id_owner) array_push($response['error_msg'], 'Owner id is required');
-            if (!count($response['error_msg']) > 0) { //cambiar esto
-                try {
-                    $animal = new Animals();
-                    $animal->id_owner = $request->id_owner;
-                    $animal->name = $request->name;
-                    $animal->id_type = $request->id_type;
-                    $animal->sex = $request->sex;
-                    $animal->age = $request->age;
-                    $animal->location = $request->location;
-                    $animal->description = $request->description;
-                    $animal->prefered_photo = $request->prefered_photo;
-                    $animal->id_breed = $request->id_breed ? $request->id_breed : null;
-                    $animal->save();
-                    $response = array('code' => 200, 'animal' => $animal, 'msg' => 'Animal created');
-                } catch (\Exception $exception) {
-                    $response = array('code' => 500, 'error_msg' => $exception->getMessage());
-                }
-            }
-        } else {
-            $response['error_msg'] = 'Nothing to create';
-        }
-        return view('createAnimal', ['response' => $response]);
-
-    }
 }
---
+++ app/Http/Controllers/AnimalControllerAdmin.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Animals;
+use Illuminate\Http\Request;
+
+class AnimalControllerAdmin extends Controller
+{
+
+    //Paginated request of all animals (Administrator)
+    public function getAnimalsAdmin()
+    {
+        $response = array('code' => 400, 'error_msg' => []);
+        try {
+            $animals = Animals::orderBy('id', 'asc')->paginate(2)->onEachSide(2);
+            if (count($animals) > 0) {
+                $response = array('code' => 200, 'animals' => $animals);
+            } else {
+                $response = array('code' => 404, 'error_msg' => ['animals not found']);
+            }
+        } catch (\Exception $exception) {
+            $response = array('code' => 500, 'error_msg' => $exception->getMessage());
+        }
+        return view('updateanimal', ['responseAnimals' => $response]);
+    }
+
+    //
+    public function createAnimalAdmin(Request $request)
+    {
+        $response = array('code' => 400, 'error_msg' => []);
+        if (isset($request)) {
+            if (!$request->name) array_push($response['error_msg'], 'Name is required');
+            if (!$request->id_type) array_push($response['error_msg'], 'Type is required');
+            if (!$request->sex) array_push($response['error_msg'], 'Sex or gender is required');
+            if (!$request->age) array_push($response['error_msg'], 'Age is required');
+            if (!$request->location) array_push($response['error_msg'], 'Location is required');
+            if (!$request->description) array_push($response['error_msg'], 'Description is required');
+            if (!$request->prefered_photo) array_push($response['error_msg'], 'Prefered_photo is required');
+            if (!$request->id_owner) array_push($response['error_msg'], 'Owner id is required');
+            if (!count($response['error_msg']) > 0) { //cambiar esto
+                try {
+                    $animal = new Animals();
+                    $animal->id_owner = $request->id_owner;
+                    $animal->name = $request->name;
+                    $animal->id_type = $request->id_type;
+                    $animal->sex = $request->sex;
+                    $animal->age = $request->age;
+                    $animal->location = $request->location;
+                    $animal->description = $request->description;
+                    $animal->prefered_photo = $request->prefered_photo;
+                    $animal->id_breed = $request->id_breed ? $request->id_breed : null;
+                    $animal->save();
+                    $response = array('code' => 200, 'animal' => $animal, 'msg' => 'Animal created');
+                } catch (\Exception $exception) {
+                    $response = array('code' => 500, 'error_msg' => $exception->getMessage());
+                }
+            }
+        } else {
+            $response['error_msg'] = 'Nothing to create';
+        }
+        return view('createanimal', ['response' => $response]);
+
+    }
+
+}
