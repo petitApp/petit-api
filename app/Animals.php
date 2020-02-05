02d6cf4d95aab834069176cb4543600d0794358e
Jose <josedlopez20@icloud.com>
2020-02-05 20:38:49 +0100

    Animal location parse bug fix

--- app/Animals.php
+++ app/Animals.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Animals extends Model
 {
-    //
+    
+        protected $casts = [
+            'longitude' => 'double',
+            'latitude' => 'double',
+          
+        ];
+    
 }
